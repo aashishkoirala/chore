3b2a142aaf542f9933e95d92dac30e509628bbba
Aashish Koirala <aashishkoirala@gmail.com>
2015-11-09 15:54:36 -0500

    TS3.

--- src/AK.Chore.Presentation/LoginServiceHostFactory.cs
+++ src/AK.Chore.Presentation/LoginServiceHostFactory.cs
@@ -73,9 +73,6 @@ namespace AK.Chore.Presentation
 
         public override ServiceHostBase CreateServiceHost(string constructorString, Uri[] baseAddresses)
         {
-            var message = baseAddresses.Select(x => x.ToString()).Aggregate((x1, x2) => x1 + "||" + x2);
-            throw new Exception(message);
-
             var serviceHost = base.CreateServiceHost(constructorString, baseAddresses);
 
             serviceHost.Description.Behaviors.Remove<ServiceCredentials>();
@@ -163,6 +160,7 @@ namespace AK.Chore.Presentation
                 var endpoint = new ServiceEndpoint(contract, binding, address);
 
                 yield return endpoint;
+                yield break;
             }
         }
 
